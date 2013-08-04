518b4a56508fb8e2ad3c615412bd23b647486f0c
shamsul <shamsul@shamsul-VPCEH28FN.(none)>
2013-08-04 13:33:19 +0530

    changed

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port  => 587,
   :domain  => 'localhost:3000',
   :user_name => "abstractsurvey@gmail.com",
-  :password => "friendaditi89",
+  :password => "",
   :authentication => 'plain',
   :enable_starttls_auto => true
 }
