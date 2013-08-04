56a9e2eb8516aaabe7eada26291714203ebb719d
shamsul <shamsul@shamsul-VPCEH28FN.(none)>
2013-08-04 13:35:59 +0530

    deleted

--- db/migrate/20130803094900_add_fields_to_candidate_form.rb
+++
@@ -1,8 +0,0 @@
-class AddFieldsToCandidateForm < ActiveRecord::Migration
-  def change
-    add_column :candidate_forms, :other_nationality, :string
-    add_column :candidate_forms, :other_visa_status, :string
-    add_column :candidate_forms, :other_language, :string
-    add_column :candidate_forms, :other_experience_in_sp, :string
-  end
-end
